9c6faa7f6c34979df927ceb8dedf91f52f05f1a6
Louis Hervé <louisdousherve@gmail.com>
2019-02-08 22:04:45 +0100

    Updated Ethernet handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
+
+#include <Ethernet.h>
 #include <SoftwareSerial.h>
+#include <SPI.h>
 
-// Constantes & Enumérations
-
+// -------- Constantes & Enumérations --------
 #define RX_RFID_PIN 4
 #define TX_RFID_PIN 5
 
-#define RX_BT_PIN 2
-#define TX_BT_PIN 3
+const byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
-enum Badge { ROUGE = 0, JAUNE, BLEU, BLEU2, BLEU3, CARTE, NON_RECONNU };
+enum Badge {
+  ROUGE = 0,
+  JAUNE,
+  BLEU,
+  BLEU2,
+  BLEU3,
+  CARTE,
+  NON_RECONNU
+};
 
 const unsigned char rouge[] = {2,'0','E','0','0','4','C','5','7','0','A','1','F',3};
 const unsigned char jaune[] = {2,'0','9','0','0','2','E','1','9','8','0','B','E',3};
@@ -17,9 +26,9 @@ const unsigned char bleu [] = {2,'1','5','0','0','9','1','9','0','8','5','9','1'
 const unsigned char bleu2[] = {2,'0','0','0','0','1','A','C','3','1','3','C','A',3};
 const unsigned char bleu3[] = {2,'1','5','0','0','8','9','1','7','4','8','C','3',3};
 const unsigned char carte[] = {2,'3','8','0','0','7','1','2','6','1','F','7','0',3};
+// -------------------------------------------
 
-// Déclaration des variables globales
-
+// ----------- Variables globales ------------
 Badge b;
 bool poules[6] = {false, false, false, false, false, false};
 int nbPoules = 0;
@@ -28,54 +37,77 @@ SoftwareSerial RFID(RX_RFID_PIN, TX_RFID_PIN);
 unsigned char buffer[64];
 int count = 0;
 
-SoftwareSerial BT(RX_BT_PIN, TX_BT_PIN);
-char bt_buffer[64];
-int bt_count = 0;
-
-String response = "";
+IPAddress ip(192, 168, 1, 48);
+EthernetServer server(80);
 
 // Prototypes des fonctions
 
 void readRfidBuffer();
-void readBtBuffer();
-Badge getBadge();
 void clearBufferArray();
-void clearBtBufferArray();
-bool arrEquals(unsigned char arr[]);
-String getChickenName(int b);
-String processRequest();
+Badge readBadge();
+bool bufferEquals(unsigned char arr[]);
+String getChickenName(int index);
 
-void setup() {
-    RFID.begin(9600);
-    BT.begin(9600);
+String generateRequestResponse();
+void handleHttpRequest();
+// -------------------------------------------
+
+void setup()
+{
     Serial.begin(9600);
+    RFID.begin(9600);
+
+    Ethernet.begin(mac, ip);
+
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Shield Ethernet non détecté. Impossible de continuer.");
+      while (true) {
+        delay(1);
+      }
+    }
+
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Le câble Ethernet est déconnecté. Impossible de continuer.");
+      while (true) {
+        delay(1);
+      }
+    }
+
+    server.begin();
+    Serial.print("Le serveur est à l'adresse ");
+    Serial.println(Ethernet.localIP());
 }
 
-void loop() {
-
+void loop()
+{
   // Lecture du badge, gestion du nombre de poules
 
-  if (RFID.available()) {
-
+  if (RFID.available())
+  {
     readRfidBuffer();
 
-    b = getBadge();
+    b = readBadge();
 
-    if (b != NON_RECONNU) {
-
+    if (b != NON_RECONNU)
+    {
       poules[b] = !poules[b];
       poules[b] ? nbPoules++ : nbPoules--;
 
       Serial.println(getChickenName(b) + " est " + (poules[b] ? "rentrée" : "sortie")
                      + "! Il y a maintenant " + nbPoules + " poule" + (nbPoules > 1 ? "s." : "."));
 
-      if (nbPoules == 6) {
+      if (nbPoules == 6)
+      {
         Serial.println("Toutes les poules sont dans le poulailler!");
       }
 
-    } else if (b == NON_RECONNU) {
+    }
+    else if (b == NON_RECONNU)
+    {
       Serial.println("Poule non reconnue.");
-    } else {
+    }
+    else
+    {
       Serial.println("Badge inconnu.");
     }
 
@@ -83,87 +115,68 @@ void loop() {
     count = 0;
   }
 
-  // Gestion des requêtes Bluetooth
-
-  if (BT.available()) {
-
-    readBtBuffer();
-
-    if (strcmp(bt_buffer, "Allo") == 0)
-    {
-      BT.println(processRequest());
-    } else {
-      Serial.println(bt_buffer);
-    }
-
-    clearBtBufferArray();
-    bt_count = 0;
-
-  }
+  // Gestion des requêtes HTTP
+  handleHttpRequest();
 
   // Délai avant la prochaine boucle (erreurs de lecture du RFID sinon, 50ms minimum)
   delay(200);
-
-  if (Serial.available())
-    RFID.write(Serial.read());
 }
 
-// Implémentations des fonctions
+// --------- Implémentations des fonctions ---------
 
-void readRfidBuffer() {
-
-  while(RFID.available()) {
+void readRfidBuffer()
+{
+  while(RFID.available())
+  {
     buffer[count] = RFID.read();
     count++;
     if(count == 64)
       break;
   }
-
 }
 
-void readBtBuffer() {
-
-  while(BT.available()) {
-    bt_buffer[bt_count] = BT.read();
-    bt_count++;
-    if(bt_count == 64)
-      break;
-  }
-
-}
-
-void clearBufferArray() {
-
+void clearBufferArray()
+{
   for (int i=0; i<count; i++)
   {
     buffer[i] = NULL;
   }
-
 }
 
-void clearBtBufferArray() {
-
-  for (int i=0; i<bt_count; i++)
+bool bufferEquals(unsigned char arr[])
+{
+  for (int i = 0; i < 14; i++)
   {
-    bt_buffer[i] = NULL;
-  }
-
-}
-
-bool arrEquals(unsigned char arr[]) {
-
-  for (int i = 0; i < 14; i++) {
     if (buffer[i] != arr[i])
       return false;
   }
 
   return true;
-
 }
 
-String getChickenName(int b) {
+Badge readBadge()
+{
+  if (bufferEquals(rouge)) {
+    return ROUGE;
+  } else if (bufferEquals(jaune)) {
+    return JAUNE;
+  } else if (bufferEquals(bleu)) {
+    return BLEU;
+  } else if (bufferEquals(bleu2)) {
+    return BLEU2;
+  } else if (bufferEquals(bleu3)) {
+    return BLEU3;
+  } else if (bufferEquals(carte)) {
+    return CARTE;
+  } else {
+    return NON_RECONNU;
+  }
+}
 
-  switch (b) {
+String getChickenName(int index)
+{
+  switch (index)
+  {
     case ROUGE:
       return "Tchiki";
       break;
@@ -183,34 +196,14 @@ String getChickenName(int b) {
       return "Bernadette";
       break;
   }
-
 }
 
-Badge getBadge() {
+String generateRequestResponse()
+{
+  String response = "{\"missing\": [";
 
-  if (arrEquals(rouge)) {
-    return ROUGE;
-  } else if (arrEquals(jaune)) {
-    return JAUNE;
-  } else if (arrEquals(bleu)) {
-    return BLEU;
-  } else if (arrEquals(bleu2)) {
-    return BLEU2;
-  } else if (arrEquals(bleu3)) {
-    return BLEU3;
-  } else if (arrEquals(carte)) {
-    return CARTE;
-  } else {
-    return NON_RECONNU;
-  }
-
-}
-
-String processRequest() {
-
-  response = "{\"missing\": [";
-
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 6; i++)
+  {
     if (!poules[i]) {
       response += "\"";
       response += getChickenName(i);
@@ -227,4 +220,38 @@ String processRequest() {
   response += String(6 - nbPoules) + "}";
 
   return response;
+}
+
+void handleHttpRequest()
+{
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    boolean currentLineIsBlank = true;
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json");
+          client.println("Connection: close");
+          client.println(); // Nouvelle ligne nécessaire (fin de l'en-tête de la réponse HTTP)
+          client.println(generateRequestResponse());
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+
+    delay(1);
+    client.stop();
+    Serial.println("Client disconnected");
+  }
 }
